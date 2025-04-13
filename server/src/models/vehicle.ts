1a5d321f5a86e27e1e8f34e3ba80a70613f63b20
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-04-13 15:46:31 +0545

    maintained the vehicle model

---
+++ server/src/models/vehicle.ts
@@ -0,0 +1,70 @@
+import { Document, Schema, model, Types } from 'mongoose';
+
+type VehicleType = 'bike' | 'scooter' | 'car' | 'others';
+
+interface VehicleInterface extends Document {
+  vehicleType: VehicleType;
+  vehicleName: string;
+  vehicleModel: string;
+  vehiclePhoto: string;
+  vehicleNumberPlate: string;
+  vehicleNumberPlatePhoto: string;
+  vehicleBlueBookPhoto: string;
+  isVehicleVerified: boolean;
+  riderId: Types.ObjectId;
+}
+
+const vehicleSchema = new Schema<VehicleInterface>(
+  {
+    vehicleType: {
+      type: String,
+      required: true,
+      enum: ['bike', 'scooter', 'car', 'others'], // 👈 enforce enum in schema too
+    },
+    vehicleName: {
+      type: String,
+      required: true,
+    },
+    vehicleModel: {
+      type: String,
+      required: true,
+    },
+    vehiclePhoto: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    vehicleNumberPlate: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    vehicleNumberPlatePhoto: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    vehicleBlueBookPhoto: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isVehicleVerified: {
+      type: Boolean,
+      default: false, // 👈 optional but often useful
+    },
+    riderId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Vehicle = model<VehicleInterface>('Vehicle', vehicleSchema);
+
+export default Vehicle;
