07792b1dab3fb1783669ef7746bb719aad4d6f38
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2025-04-20 16:42:43 +0545

    Refactor findRide function to use IRequest type for request parameter and clean up commented user validation code

--- server/src/controllers/rides.ts
+++ server/src/controllers/rides.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
 import Ride from '../models/rides';
+import IRequest from '../middleware/IRequest';
 
-export const findRide = async (req: Request, res: Response) => {
+export const findRide = async (req: IRequest, res: Response) => {
   try {
+    
     const { email, start_location, destination } = req.body;
 
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ success: false, message: 'User not found with the provided email' });
-    }
-
+    // const user = await User.findOne({ email });
+    // if (!user) {
+    //   return res.status(404).json({ success: false, message: 'User not found with the provided email' });
+    // }
+    const customerId = req.userId; 
     const ride = await Ride.create({
-      customerId: user._id,
+      customerId,
       start_location,
       destination,
       status: 'not-started',
