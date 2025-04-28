30b255ea971442bd79217825f5a39633cd7e6e4a
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-04-28 16:15:03 +0545

    created model of RiderList

--- server/src/models/riderLIst.ts
+++ server/src/models/riderLIst.ts
@@ -1,5 +1,5 @@
 
-import {Schema, Types } from 'mongoose'
+import {model, Schema, Types } from 'mongoose'
 
 interface IRiderList extends Document{
     riderId:Types.ObjectId,
@@ -9,7 +9,7 @@ interface IRiderList extends Document{
     | 'not-accepted',
 }
 
-const RiderList= new Schema <IRiderList>({
+const RiderListSchema= new Schema <IRiderList>({
     riderId:{
         type:Schema.Types.ObjectId,
         required:true
@@ -17,5 +17,17 @@ const RiderList= new Schema <IRiderList>({
     rideId:{
         type:Schema.Types.ObjectId,
         required:true
+    },
+    status:{
+        type:String,
+        enum:['accepted','not-accepted'],
+        default:'not-accepted'
     }
+},{
+    timestamps:true
 })
+
+const RiderList= model <IRiderList>('RideList',RiderListSchema)
+
+
+export default RiderList
