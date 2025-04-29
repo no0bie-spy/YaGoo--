3d872dadf458ee94a03724a04dac694912466128
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2025-04-29 15:51:53 +0545

    comments added to controllers and routes

--- server/src/routes/rides.ts
+++ server/src/routes/rides.ts
@@ -5,15 +5,15 @@ import rideController from '../controllers/rides';
 
 const rideRouter = express.Router();
 
-rideRouter.post('/create', validate(rideValidation.findRide), rideController.createRideRequest);
-rideRouter.post('/bid', validate(rideValidation.placeBid), rideController.submitBid);
-rideRouter.post('/rider-request', validate(rideValidation.requestRideByRider), rideController.requestRideAsRider)
-rideRouter.get('/requests',  rideController.getAllRequestedRides);
-rideRouter.get('/available-riders',  rideController.getAvailableRiders);
-rideRouter.get('/verify-ride-otp',rideController.verifyRideOtp);
-rideRouter.post('/accept',rideController.acceptRideRequestByCustomer);
-rideRouter.delete('/reject-rider',rideController.rejectRider);
-rideRouter.post('/complete-ride',rideController.completedRide)
-rideRouter.post('/submit-ride-review',rideController.submitRideReview)
+rideRouter.post('/create', validate(rideValidation.findRide), rideController.createRideRequest);//successfully create ride by customer
+rideRouter.post('/bid', validate(rideValidation.placeBid), rideController.submitBid);//customer place bids and send
+rideRouter.get('/requests',  rideController.getAllRequestedRides);//rider gets all the ride requests from customer
+rideRouter.post('/rider-request', validate(rideValidation.requestRideByRider), rideController.requestRideAsRider)//Rider requests the ride
+rideRouter.get('/available-riders',  rideController.getAvailableRiders); //customer gets all the requests from riders
+rideRouter.post('/accept',rideController.acceptRideRequestByCustomer); //customer accepts one rider and send otp to rider
+rideRouter.delete('/reject-rider',rideController.rejectRider); //customer rejects rider
+rideRouter.get('/verify-ride-otp',rideController.verifyRideOtp); //customer verifies rider by otp
+rideRouter.post('/complete-ride',rideController.completedRide) //ride completes
+rideRouter.post('/submit-ride-review',rideController.submitRideReview) //customer sends ride review
 
 export default rideRouter;
