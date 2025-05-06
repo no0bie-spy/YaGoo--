f92fc9b681be86ed77346b09997fd52d654d91f2
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-05-06 12:23:10 +0545

    Implement payment handling in ViewOtpScreen and update navigation parameters from RiderDashboard

--- app/(root)/(rides)/ViewOtpScreen.tsx
+++ app/(root)/(rides)/ViewOtpScreen.tsx
@@ -1,15 +1,65 @@
+import AppButton from '@/components/Button';
+import { getSession } from '@/usableFunction/Session';
+import axios from 'axios';
+import { router, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 
 const ViewOtpScreen = () => {
-    // Replace with actual OTP data and logic
-    const otp = '123456';
+    const IP_Address = process.env.EXPO_PUBLIC_ADDRESS;
+    const { rideId, message } = useLocalSearchParams();
 
+    const [errors, setErrors] = React.useState<string[]>([]);
+    const handleReceivePayment = async () => {
+        try {
+            const token = await getSession('accessToken');
+            if (!token) {
+                Alert.alert('You are not logged in. Please log in to continue.');
+                return;
+            }
+
+            const response = await axios.post(`http://${IP_Address}:8002/rides/received-payment`, {
+                rideId,
+            }, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+
+            const data=await response.data;
+            console.log("Received payment response:", data);
+            if (data.message) {     
+                
+                if (data.status === 'success') {
+                    Alert.alert('Payment received successfully.');
+                    router.push('/(root)/(tabs)/home');
+                } else {
+                    Alert.alert('Failed to receive payment.');
+                }
+            }
+           
+        }
+        catch (error: any) {
+            console.error('Fetch ride requests error:', error);
+            if (error.response?.data?.details && Array.isArray(error.response.data.details)) {
+                setErrors(error.response.data.details.map((err: any) => err.message));
+            } else if (error.response?.data?.message) {
+                setErrors([error.response.data.message]);
+            } else {
+                setErrors(['Something went wrong.']);
+            }
+        }
+
+
+    }
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Your OTP</Text>
-            <Text style={styles.otp}>{otp}</Text>
-            <Text style={styles.instruction}>Please share this OTP with your rider.</Text>
+            <Text style={styles.otp}> </Text>
+            <Text style={styles.instruction}>Please share  OTP with your customer.</Text>
+            <Text style={styles.instruction}>This OTP is valid for 10 minutes.</Text>
+            <AppButton
+                title="Receive Payment"
+                onPress={handleReceivePayment}
+            />
         </View>
     );
 };
