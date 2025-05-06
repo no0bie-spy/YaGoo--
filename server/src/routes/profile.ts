726be872f68f2cb1fca6f890cff2e6e531a2df81
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2025-05-06 16:09:29 +0545

    managed routes for profile

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,10 +1,31 @@
 import { Router } from "express";
 import profileController from "../controllers/profile"
+import validate from "../middleware/validation";
+import userValidation from "../validations/auth";
+
 
 const profileRouter = Router();
+/**
+ * GET /profile/history
+ * Retrieve the ride history of the logged-in user (customer or rider).
+ */
+profileRouter.get('/history', profileController.viewHistory);
 
-profileRouter.get("/view-history",profileController.viewHistory)
+/**
+ * GET /profile/details
+ * Fetch the profile information of the currently logged-in user.
+ */
+profileRouter.get('/details', profileController.userDetails);
 
+/**
+ * PUT /profile/edit
+ * Update the logged-in user's profile information after validation.
+ */
+profileRouter.put(
+  '/edit',
+  validate(userValidation.editProfileDetails),
+  profileController.editProfileDetails
+);
 
 export default profileRouter;
 
