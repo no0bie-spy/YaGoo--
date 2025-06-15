6311db0df4cc0aeffb2a768b5e5c6b61d5385ff0
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-06-15 13:39:34 +0545

    updated

--- server/src/models/chat.ts
+++ server/src/models/chat.ts
@@ -1,34 +1,38 @@
-import { Document, Schema, Types, model } from "mongoose";
+import { Document, Schema, Types, model } from 'mongoose';
 
 // Interface
 export interface IChat extends Document {
   message: string;
-  userId: Types.ObjectId;
+  userId: Types.ObjectId[];
   roomId: Types.ObjectId;
   createdAt: Date;
 }
 
 // Schema
-const ChatSchema = new Schema<IChat>({
-  message: {
-    type: String,
-    required: true,
+const ChatSchema = new Schema<IChat>(
+  {
+    message: {
+      type: String,
+      required: true,
+    },
+    userId: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User', // reference to User model
+        required: true,
+      },
+    ],
+    roomId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Room', // reference to Room model
+      required: true,
+    },
   },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User", // reference to User model
-    required: true,
+  {
+    timestamps: { createdAt: true, updatedAt: false }, // only createdAt
   },
-  roomId: {
-    type: Schema.Types.ObjectId,
-    ref: "Room", // reference to Room model
-    required: true,
-  },
-}, {
-  timestamps: { createdAt: true, updatedAt: false } // only createdAt
-});
+);
 
+const Chat = model<IChat>('Chat', ChatSchema);
 
-const Chat = model<IChat>("Chat", ChatSchema);
-
-export default Chat
+export default Chat;
